bc8b97c7492e66becedeecd21f53c21468412f23
Rikin Gurditta <dolllyamar@gmail.com>
2023-04-04 03:14:47 -0400

    Label graphs

--- project.m
+++ project.m
@@ -59,25 +59,26 @@ betas = smoothdata(betas);
 % [~, x_betas_week] = SIRBetas(x_data_week(1, :)', t_week, betas, GAMMA, t_week);
 
 %% plot1
-subplot(3, 1, 1)
-plot(dates, x_data(:, 1), '-r', dates, x_data(:, 2), '-g', dates, x_data(:, 3), '-b')
-% plot(t_data, x_data(:, 2), '-g', t_data, x_data(:, 3), '-b')
-% plot(t_data, x_data(:, 2), '-g')
-
-subplot(3, 1, 2)
-plot(dates, betas, '-k')
-% plot(dates_week, betas, '-k')
-
-subplot(3, 1, 3)
-% figure()
-plot(dates, x_betas(:, 1), '-r', dates, x_betas(:, 2), '-g', dates, x_betas(:, 3), '-b')
-% plot(dates_week, x_betas_week(:, 1), '-r', dates_week, x_betas_week(:, 2), '-g', dates_week, x_betas_week(:, 3), '-b')
-% plot(t_data, x_betas(:, 2), '-g', t_data, x_betas(:, 3), '-b')
-% plot(t_data, x_betas(:, 2), '-g')
+% subplot(3, 1, 1)
+% plot(dates, x_data(:, 1), '-r', dates, x_data(:, 2), '-g', dates, x_data(:, 3), '-b')
+% % plot(t_data, x_data(:, 2), '-g', t_data, x_data(:, 3), '-b')
+% % plot(t_data, x_data(:, 2), '-g')
+% 
+% subplot(3, 1, 2)
+% plot(dates, betas, '-k')
+% % plot(dates_week, betas, '-k')
+% 
+% subplot(3, 1, 3)
+% % figure()
+% plot(dates, x_betas(:, 1), '-r', dates, x_betas(:, 2), '-g', dates, x_betas(:, 3), '-b')
+% % plot(dates_week, x_betas_week(:, 1), '-r', dates_week, x_betas_week(:, 2), '-g', dates_week, x_betas_week(:, 3), '-b')
+% % plot(t_data, x_betas(:, 2), '-g', t_data, x_betas(:, 3), '-b')
+% % plot(t_data, x_betas(:, 2), '-g')
 
 %% plot2
 
-figure()
+fig = figure();
+compartments = {'S', 'I', 'R'};
 for i=1:3
     subplot(3, 1, i)
     p = plot(dates, x_data(:, i), '-k', dates, x_betas(:, i), '-k');
@@ -86,20 +87,57 @@ for i=1:3
     v(i) = 1;
     set(p(1), 'Color', v * 0.5);
     set(p(2), 'Color', v);
+    title(strcat(compartments{i}, ' compartment vs time'))
+    xlabel('time (days)'), ylabel('people')
+    legend({'data', 'simulated'})
 end
+
+exportgraphics(fig, 'SIR.png', 'Resolution', 300)
 
 %% plot3
 
-figure()
+fig = figure();
 subplot(3, 1, 1)
 plot(dates, betas, '-k')
+title('β vs time')
+xlabel('time (days)'), ylabel('β (1/people days)')
 % plot(dates_week, betas, '-k')
 
 subplot(3, 1, 2)
 plot(dates, d_gamma * GAMMA / betas, '-k')
+title('sensitivity of β to γ vs time')
+xlabel('time (days)'), ylabel('S(β, γ)')
 
 subplot(3, 1, 3)
 p = plot(dates, x_data(:, 2), '-k', dates, x_betas(:, 2), '-k');
 % p = plot(dates, x_data(:, 2), '-k', dates_week, x_betas_week(:, 2), '-k');
 set(p(1), 'Color', [0, 0.5, 0]);
 set(p(2), 'Color', [0, 1, 0]);
+title('I compartment vs time')
+xlabel('time (days)'), ylabel('I (people)')
+legend({'data', 'simulated'})
+
+exportgraphics(fig, 'beta.png', 'Resolution', 300)
+
+
+%% fit both params
+% p0 = [betas(1); GAMMA];
+% [params, dp_dx] = FindParams(t_data, x_data, p0);
+% [~, x_params] = SIRParams(x_data(1, :)', t_data, params, t_data);
+% 
+% figure()
+% subplot(3, 1, 1)
+% plot(dates, x_data(:, 1), '-r', dates, x_data(:, 2), '-g', dates, x_data(:, 3), '-b')
+% 
+% subplot(3, 1, 2)
+% yyaxis left
+% plot(dates, params(:, 1), '-k')
+% yyaxis right
+% plot(dates, params(:, 2), '-r')
+% 
+% subplot(3, 1, 3)
+% % figure()
+% plot(dates, x_params(:, 1), '-r', dates, x_params(:, 2), '-g', dates, x_params(:, 3), '-b')
+% % plot(dates_week, x_betas_week(:, 1), '-r', dates_week, x_betas_week(:, 2), '-g', dates_week, x_betas_week(:, 3), '-b')
+% % plot(t_data, x_betas(:, 2), '-g', t_data, x_betas(:, 3), '-b')
+% % plot(t_data, x_betas(:, 2), '-g')
